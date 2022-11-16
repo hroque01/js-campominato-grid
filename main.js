59683bf5cb867228ecd4bf1e86b3669eb3db6b05
hroque01 <hadson.roque01@gmail.com>
2022-11-16 15:29:51 +0100

    terminato

--- main.js
+++ main.js
@@ -8,42 +8,41 @@ const btn = document.querySelector(".btn")
 
 // creo la griglia con un ciclo
 
-for ( i = 1; i < 100; i++ ) {
+for (let i = 1; i < 101; i++) {
 
     btn.addEventListener("click",
 
         function (){
+
             let nuovoElemento = createBox();
+            
             nuovoElemento.addEventListener("click",
 
             function () {
                 nuovoElemento.classList.toggle("clicked");
+                console.log(i)
             }
         
         )
 
         container.appendChild(nuovoElemento)
+
+        function createBox() {
+
+            // creo un elemento div
+            const mioElemnt = document.createElement('div');
+            mioElemnt.innerHTML = `${[i]}`
+            
+            // aggiungo la classe box
+            mioElemnt.classList.add('box');
+        
+            // lo metto nel dom
+            return mioElemnt;
+        
+        
+        };
     })
     
-
-   
-
-    
-
-    
 };
 
-function createBox() {
 
-    // creo un elemento div
-    const mioElemnt = document.createElement('div');
-    mioElemnt.innerHTML = `${[i]}`
-    
-    // aggiungo la classe box
-    mioElemnt.classList.add('box');
-
-    // lo metto nel dom
-    return mioElemnt;
-
-
-};
