ce7ef56bad7b07a3eacc06e9b0ba25885440af8f
hroque01 <hadson.roque01@gmail.com>
2022-11-16 17:52:59 +0100

    Consegna esercizio normale

--- main.js
+++ main.js
@@ -5,35 +5,12 @@ const container = document.querySelector(".grid_container")
 // dichiaro il pulsante
 const btn = document.querySelector(".btn")
 
-// dichiaro la tendina
-const tendina = document.getElementById('difficulty');
-
-let valX;
-
-if ( tendina.value === "ez") {
-    valX = 31;
-};
-if ( tendina.value === "medium") {
-    valX= 51;
-};
-if ( tendina.value === "pro") {
-    valX= 101;
-};
-
-
-
 
 // creo la griglia con un ciclo
 
-for (let i = 1; i < valX; i++) {
+for (let i = 1; i < 101; i++) {
 
-    
-    
     btn.addEventListener("click",
-
-        function () {
-            container.innerHTML = "";
-        }
 
         function (){
 
@@ -44,7 +21,9 @@ for (let i = 1; i < valX; i++) {
             function () {
                 nuovoElemento.classList.toggle("clicked");
                 console.log(i)
-            })
+            }
+        
+        )
 
         container.appendChild(nuovoElemento)
 
@@ -62,7 +41,7 @@ for (let i = 1; i < valX; i++) {
         
         
         };
-    });
+    })
     
 };
 
