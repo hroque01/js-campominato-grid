fb04fa0d43f86b46b40245e817b306fb2a26e64a
hroque01 <hadson.roque01@gmail.com>
2022-11-17 10:39:35 +0100

    adjustment lvl difficulty

--- main.js
+++ main.js
@@ -1,48 +1,68 @@
 
 // dichiaro il contenitore
-const container = document.querySelector(".grid_container")
+const container = document.querySelector(".grid_container");
 
 // dichiaro il pulsante
-const btn = document.querySelector(".btn")
+const btn = document.querySelector(".btn");
+
+// dichiaro la difficolta'
+const difficulty = document.getElementById('difficulty');
 
 
-// creo la griglia con un ciclo
+// il bottone deve generare la griglia
 
-for (let i = 1; i < 101; i++) {
+btn.addEventListener('click',
 
-    btn.addEventListener("click",
+    function(){
 
-        function (){
+        container.innerHTML="";
 
-            let nuovoElemento = createBox();
-            
-            nuovoElemento.addEventListener("click",
-
-            function () {
-                nuovoElemento.classList.toggle("clicked");
-                console.log(i)
-            }
-        
-        )
-
-        container.appendChild(nuovoElemento)
-
-        function createBox() {
-
-            // creo un elemento div
-            const mioElemnt = document.createElement('div');
-            mioElemnt.innerHTML = `${[i]}`
-            
-            // aggiungo la classe box
-            mioElemnt.classList.add('box');
-        
-            // lo metto nel dom
-            return mioElemnt;
-        
-        
+        // impostiamo la difficolta e il livello tramite griglia
+        let level = 0;
+        if (difficulty.value === "ez") {
+            level = 101;
         };
-    })
-    
-};
+        if (difficulty.value === "medium") {
+            level = 82;
+        };
+        if (difficulty.value === "hard") {
+            level = 50;
+        };
+        
+        // ora creiamo il ciclo infinito
+        for ( let i = 1; i < level; i++ ){
+
+              // creo l'elemento div
+            const mioElemnt = document.createElement('div');
+            mioElemnt.innerHTML += `${i}`;
+            container.append(mioElemnt);
+
+            // aggiungiamo la classe box
+            mioElemnt.classList.add('box');
+
+            //aggiungo la classe toggle
+            mioElemnt.addEventListener('click',
+                function(){
+                    mioElemnt.classList.toggle('clicked');
+                    console.log(i)
+                });
+
+            if (difficulty.value === "ez") {
+                level = 101;
+                mioElemnt.classList.add('width-easy')
+            };
+            if (difficulty.value === "medium") {
+                level = 82;
+                mioElemnt.classList.add('width-medium')
+            };
+            if (difficulty.value === "hard") {
+                level = 50;
+                mioElemnt.classList.add('width-hard')
+            };
+            
+        };
+});
+
+
 
 
